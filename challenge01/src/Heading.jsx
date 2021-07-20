5386da635635465c9464e719dc939558d4abebac
Kumar Shantanu <76111005+kumarshantanu01@users.noreply.github.com>
2021-07-20 20:58:11 +0530

    Add files via upload

---
+++ challenge01/src/Heading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Heading(){
+    return
+    <h1>My favourite web series</h1>;
+}
+
+export default Heading;
---
+++ challenge01/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <>
+  <Heading />
+  <p>Top 5 series </p>
+  <ol>
+    <li>Game of thrones</li>
+    <li>Peaky blinders</li>
+    <li>Money heist</li>
+    <li>dark</li>
+    <li>The familyman</li>
+  </ol>
+  </>,
+
+  document.getElementById("root")
+)
+
+
