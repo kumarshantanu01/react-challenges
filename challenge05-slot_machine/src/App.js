8813348f3e680fe550c32bf548bc6648975ee338
Kumar Shantanu <76111005+kumarshantanu01@users.noreply.github.com>
2021-07-26 02:22:21 +0530

    Add files via upload

---
+++ challenge05-slot_machine/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SlotM from './SlotMach';
+
+const App = () => {
+return(
+<>
+<h1 className="heading_style">
+ Welcome to {" "} 
+ <span style= { { fontWeight : 'bold' } }> Slot machine game </span> {" "}  
+</h1>
+
+<div className= 'slotmachine'>
+ <SlotM x='😀' y='😀' z='😀'/>
+ <SlotM x='🎶' y='🎶' z='🎶'/>
+ <SlotM x='📍' y='🎶' z='📍'/>
+</div>
+</>
+);
+};
+
+
+export default App;
