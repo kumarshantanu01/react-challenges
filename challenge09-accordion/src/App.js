ac6d5dd794edb468175bf6722f7893a9be400660
Kumar Shantanu <76111005+kumarshantanu01@users.noreply.github.com>
2021-07-30 13:20:13 +0530

    Add files via upload

---
+++ challenge09-accordion/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Accordian from './component/accordian/Accordian';
+import accordian from './component/accordian/accordian.css';
+
+const App = () => {
+    return(
+        <>
+        <Accordian />
+        </>
+    )
+
+}
+
+export default App
