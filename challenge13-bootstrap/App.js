1920e46dbead1f1880dca9fa49545c83b35e1a18
Kumar Shantanu <76111005+kumarshantanu01@users.noreply.github.com>
2021-09-16 19:50:38 +0530

    Add files via upload

---
+++ challenge13-bootstrap/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+const App = () => {
+  return(
+    <>
+    <h1>welcome</h1>
+    <div className="container">
+  <div className="row">
+    <div className="col">
+      <div class="card" >
+  <img src="https://picsum.photos/200/199" class="card-img-top" alt="..." height="200px" width="200px"/>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col">
+      <div class="card" >
+  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col">
+      <div class="card" >
+  <img src="https://picsum.photos/200/201" class="card-img-top" alt="..." height="200px" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+    </>
+  )
+}
+export default App;
